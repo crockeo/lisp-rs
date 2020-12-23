340d1cded6115f9daa1a334ed6f41de25a7b3581
Cerek Hillen <chillen@lyft.com>
2020-12-23 10:20:24 -0500

    fix macro bug

--- src/macro_defs.rs
+++ src/macro_defs.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! lisp_error {
-    ( $fmt:expr, $( $arg:expr ),* ) => {
+    ( $fmt:expr $(, $arg:expr ),* ) => {
         use crate::error::LispError;
         return Err(LispError::new(format!($fmt, $( $arg )*)));
     };
